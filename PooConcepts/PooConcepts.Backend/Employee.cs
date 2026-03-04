c9183b10d2eba7b3b447bb6b3a60f2c9a76b27e8
LUIS ALEJANDRO ALZATE RAMIREZ <luisalzate1158182@correo.itm.edu.co>
2026-03-04 18:13:46 -0500

    Add Salary Employee

--- PooConcepts/PooConcepts.Backend/Employee.cs
+++ PooConcepts/PooConcepts.Backend/Employee.cs
@@ -2,36 +2,35 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace PooConcepts.Backend
+namespace PooConcepts.Backend;
+
+public abstract class Employee
 {
-    public abstract class Employee
+    //  Constructors
+    protected Employee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate)
     {
-        // Constructors
-        protected Employee(int id, string firstName, string lastName, bool isActive, Date bornDate, Date hireDate)
-        {
-            Id = id;
-            FirstName = firstName;
-            LastName = lastName;
-            IsActive = isActive;
-            BornDate = bornDate;
-            HireDate = hireDate;
-        }
-
-        // Properties
-        public int Id { get; set; }
-        public string FirstName { get; set; } = null!;
-        public string LastName { get; set; } = null!;
-        public bool IsActive { get; set; }
-        public Date BornDate { get; set; } = null!; 
-        public Date HireDate { get; set; } = null!;
-
-        // Methods
-        public override string ToString()
-        {
-            return $"{Id}\t{FirstName} {LastName}\n\t" +
-                   $"Value to pay: {GetValueToPay()}";
-        }
-        public abstract decimal GetValueToPay();
-        
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        IsActive = isActive;
+        BornDate = bornDate;
+        HireDate = hireDate;
     }
+
+    // Properties
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public bool IsActive { get; set; }
+    public Date BornDate { get; set; } = null!;
+    public Date HireDate { get; set; } = null!;
+
+    // Methods
+    public override string ToString()
+    {
+        return $"{Id}\t{FirstName} {LastName}\n\t" +
+                $"Value to pay.....: {GetValueToPay(),20:C2}";
+    }
+
+    public abstract decimal GetValueToPay();
 }
--- PooConcepts/PooConcepts.Frontend/Program.cs
+++ PooConcepts/PooConcepts.Frontend/Program.cs
@@ -2,10 +2,12 @@
 
 try
 {
-
+    var employee1 = new SalaryEmployee(1, "Maria", "Perez", true, new Date(1990, 5, 15), new Date(2015, 3, 1), 2500000);
+    var employee2 = new SalaryEmployee(1, "Joaquín", "Gonzales", true, new Date(1980, 3, 5), new Date(2010, 11, 15), 10395876);
+    Console.WriteLine(employee1);
+    Console.WriteLine(employee2);
 }
 catch (Exception ex)
 {
-
     Console.WriteLine(ex.Message);
 }
